5d10d85134ceb7af2065e0ae8089bf5c2aafc303
Kevin Sanchez <kevisanchez@alumno.unlam.edu.ar>
2021-05-17 23:13:02 -0300

    Primer Commit

---
+++ app/src/main/java/com/example/workingapp/ViewTicketActivity.kt
@@ -0,0 +1,17 @@
+package com.example.workingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewTicketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewticket)
+
+
+
+    }
+
+
+
+}
