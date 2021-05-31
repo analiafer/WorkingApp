ec510c66920f9d647c77a96804e418742032cc3f
Kevin Sanchez <kevisanchez@alumno.unlam.edu.ar>
2021-05-31 01:17:13 -0300

    Ya se puede ir a la pantalla del ticket y volver atrás. Falta mejorar el contenido del ticket en la visualización.

--- app/src/main/java/com/example/workingapp/MainActivity.kt
+++ app/src/main/java/com/example/workingapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.workingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnViewTicket: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setViews()
+        setListeners()
+
     }
+
+    private fun setViews() {
+        btnViewTicket = findViewById(R.id.btnViewTicket)
+    }
+
+    private fun setListeners() {
+        btnViewTicket.setOnClickListener {
+            navigateToTicket()
+        }
+    }
+
+    private fun navigateToTicket() {
+        val intent = Intent(this, ViewTicketActivity::class.java)
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/com/example/workingapp/ViewTicketActivity.kt
+++ app/src/main/java/com/example/workingapp/ViewTicketActivity.kt
@@ -2,16 +2,29 @@ package com.example.workingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ViewTicketActivity : AppCompatActivity() {
+    lateinit var btnCloseTicket: Button
+    lateinit var btnDeleteTicket: Button
+    lateinit var btnEditTicket: Button
+    lateinit var btnBack: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_ticket)
 
-
+        setViews()
+        setListeners()
 
     }
 
+    private fun setViews() {
+        btnBack = findViewById(R.id.buttonBackArrow)
+    }
 
-
+    private fun setListeners() {
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
 }
