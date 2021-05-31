2cde5f4bd305d15a2fe943caf717ed538103f7d3
Kevin Sanchez <kevisanchez@alumno.unlam.edu.ar>
2021-05-31 02:26:57 -0300

    Ya se puede ir a la pantalla del ticket y volver atrás. Falta mejorar el contenido del ticket en la visualización.

--- app/src/main/java/com/example/workingapp/MainActivity.kt
+++ app/src/main/java/com/example/workingapp/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnViewTicket: Button
+    lateinit var btnViewTicket: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
