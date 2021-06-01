a282fa048e2b8a03448b4f8f9ee601b0d9ca34a6
Kevin Sanchez <kevisanchez@alumno.unlam.edu.ar>
2021-06-01 00:56:39 -0300

    Ya se puede ir a la pantalla del ticket y volver atrás. Falta mejorar el contenido del ticket en la visualización.

--- app/src/main/java/com/example/workingapp/MainActivity.kt
+++ app/src/main/java/com/example/workingapp/MainActivity.kt
@@ -8,29 +8,10 @@ import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnViewTicket: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setViews()
-        setListeners()
-
-    }
-
-    private fun setViews() {
-        btnViewTicket = findViewById(R.id.btnViewTicket)
-    }
-
-    private fun setListeners() {
-        btnViewTicket.setOnClickListener {
-            navigateToTicket()
-        }
-    }
-
-    private fun navigateToTicket() {
-        val intent = Intent(this, ViewTicketActivity::class.java)
-        startActivity(intent)
     }
 
 }
--- app/src/main/java/com/example/workingapp/ViewTicketActivity.kt
+++ app/src/main/java/com/example/workingapp/ViewTicketActivity.kt
@@ -3,12 +3,13 @@ package com.example.workingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class ViewTicketActivity : AppCompatActivity() {
     lateinit var btnCloseTicket: Button
     lateinit var btnDeleteTicket: Button
     lateinit var btnEditTicket: Button
-    lateinit var btnBack: Button
+    lateinit var btnBack: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_ticket)
