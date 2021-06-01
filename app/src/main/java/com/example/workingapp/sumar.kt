7f8742d762ec61e0726ffb25805ebafef9067d81
Ari <arianavargas235@hotmail.com>
2021-06-01 17:42:49 -0300

    hice la pantalla agregar (:

---
+++ app/src/main/java/com/example/workingapp/sumar.kt
@@ -0,0 +1,11 @@
+package com.example.workingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sumar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sumar)
+    }
+}
