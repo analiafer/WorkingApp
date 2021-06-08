b61adbfbbed36fc5055f89d83a07c2ac66341969
Kevin Sanchez <kevisanchez@alumno.unlam.edu.ar>
2021-06-08 17:38:24 -0300

    Arreglado el activity Sumar (la pantalla estaba vacia de código). Renombrados el activity sumar (activity_new_ticket.xml) Renombrado la clase sumar (NewTicketActivity.kt) Eliminado la estructura del ticket en activity_view_ticket.xml y agregada en row_ticket.xml

--- app/src/main/java/com/example/workingapp/NewTicketActivity.kt
+++ app/src/main/java/com/example/workingapp/NewTicketActivity.kt
@@ -3,9 +3,9 @@ package com.example.workingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class sumar : AppCompatActivity() {
+class NewTicketActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sumar)
+        setContentView(R.layout.activity_new_ticket)
     }
 }
