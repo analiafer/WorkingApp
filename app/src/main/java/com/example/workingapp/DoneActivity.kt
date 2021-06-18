7c56fb7cfe2249dce39c30d79628a1113c3508c5
Ari <arianavargas235@hotmail.com>
2021-06-18 14:25:23 -0300

    Sync with master

--- app/src/main/java/com/example/workingapp/DoneActivity.kt
+++ app/src/main/java/com/example/workingapp/DoneActivity.kt
@@ -3,13 +3,13 @@ package com.example.workingapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
 import com.example.workingapp.databinding.ActivityDoneBinding
-import com.example.workingapp.recyclerView.Ticket
-import com.example.workingapp.recyclerView.TicketAdapter
 
-class DoneActivity : AppCompatActivity(), TicketAdapter.OnTicketClickListener {
+class DoneActivity : AppCompatActivity() {
     private lateinit var bindingDone: ActivityDoneBinding
+    private lateinit var viewTicket: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +18,23 @@ class DoneActivity : AppCompatActivity(), TicketAdapter.OnTicketClickListener {
         var appbarnav = bindingDone.tbTicket
         setSupportActionBar(appbarnav)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setViews()
         setListener()
-        setupRecyclerView()
+
+        /*Ari recycler
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+         recycler.adapter =*/
+    }
+
+    private fun setViews() {
+        viewTicket = findViewById(R.id.viewTicket)
     }
 
     private fun setListener() {
+        viewTicket.setOnClickListener {
+            var intent = Intent(this, ViewTicketActivity::class.java)
+            startActivity(intent)
+        }
 
         val navigationBottom = bindingDone.bottomNavigation
         navigationBottom.selectedItemId = R.id.option_realizados
@@ -61,6 +73,7 @@ class DoneActivity : AppCompatActivity(), TicketAdapter.OnTicketClickListener {
         startActivity(general)
     }
 
+
     private fun optionRealizado() {
 
         val realizado = Intent(this, DoneActivity::class.java)
@@ -78,33 +91,4 @@ class DoneActivity : AppCompatActivity(), TicketAdapter.OnTicketClickListener {
         val cancel = Intent(this, CancelActivity::class.java)
         startActivity(cancel)
     }
-
-    private fun setupRecyclerView() {
-
-        val recyclerViewMain = bindingDone.recyclerViewDone
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerViewMain.layoutManager = layoutManager
-
-        var listTicket = listOf(
-            Ticket("Soy un ticket", "Soy una descripcion", "Autor", 1),
-            Ticket("Soy un ticket 2", "Soy una descripcion 2", "Autor 2", 2),
-            Ticket("Soy un ticket 3", "Soy una descripcion 3", "Autor 3", 3),
-            Ticket("Soy un ticket 4", "Soy una descripcion 4", "Autor 4", 4),
-            Ticket("Soy un ticket 5", "Soy una descripcion 5", "Autor 5", 5),
-            Ticket("Soy un ticket 6", "Soy una descripcion 6", "Autor 6", 6),
-            Ticket("Soy un ticket 7", "Soy una descripcion 7", "Autor 7", 7),
-            Ticket("Soy un ticket 8", "Soy una descripcion 8", "Autor 8", 8),
-            Ticket("Soy un ticket 9", "Soy una descripcion 9", "Autor 9", 9),
-            Ticket("Soy un ticket 10", "Soy una descripcion 10", "Autor 10", 10),
-            Ticket("Soy un ticket 11", "Soy una descripcion 11", "Autor 11", 11),
-            Ticket("Soy un ticket 12", "Soy una descripcion 12", "Autor 12", 12)
-        )
-        recyclerViewMain.adapter = TicketAdapter(listTicket, this)
-        }
-
-        override fun onItemClick() {
-            var intent = Intent(this, ViewTicketActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
+}
