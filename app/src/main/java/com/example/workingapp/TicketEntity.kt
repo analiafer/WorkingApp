fc89b6d4d51bfac96657f98ba18998792cd8ec1c
analiafer <fernandezanalia39@gmail.com>
2021-06-26 00:22:11 -0300

    implemento room, creo la clase dao y ticket entity

---
+++ app/src/main/java/com/example/workingapp/TicketEntity.kt
@@ -0,0 +1,24 @@
+package com.example.workingapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "ticket")
+data class TicketEntity (
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id : Long = 0,
+
+    @ColumnInfo(name = "titulo")
+    val titulo: String,
+
+    @ColumnInfo(name = "autor")
+    val autor : String,
+
+    @ColumnInfo(name = "descripcion")
+    val descripcion : String
+)
+
