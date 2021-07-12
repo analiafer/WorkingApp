e3210dc8fa21291255e8bb9c309d1d88b6981f81
Lucas Lopez <lucasaxellopez3@gmail.com>
2021-07-12 17:52:55 -0300

    Implementando repositorios y adaptando llamada api

--- app/src/main/java/com/example/workingapp/data/service/API.kt
+++ app/src/main/java/com/example/workingapp/data/service/API.kt
@@ -1,25 +1,19 @@
 package com.example.workingapp.data.service
 
-import com.example.workingapp.model.WeatherModel
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class API {
+object API {
 
-    private  fun getAPI(): climaAPI{
+    val baseUrl = "https://api.openweathermap.org/data/2.5/"
 
-        val baseUrl = "http://api.openweathermap.org/data/2.5/"
-        val retrofit: Retrofit = Retrofit.Builder()
+    val service by lazy{
+        Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        return retrofit.create(climaAPI::class.java)
+            .create(climaAPI::class.java)
     }
-
-    fun getCurrentWeatherData(ciudad: String,accessKey: String, callback: Callback<WeatherModel>) {
-        getAPI().getCurrentWeatherData(ciudad, accessKey).enqueue(callback)
-    }
-
 
 }
